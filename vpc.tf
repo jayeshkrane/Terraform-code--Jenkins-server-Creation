b36b916e2f6df856b8e051428d2c3dfddd7f7a96
Jayesh Rane <159913092+jayeshkrane@users.noreply.github.com>
2026-04-05 10:19:30 +0530

    vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -2,12 +2,14 @@ resource "aws_vpc" "myapp-vpc" {
   cidr_block = var.vpc_cidr_block
   tags = {
     Name = "${var.env_prefix}-vpc"
+    Environment = var.env_prefix
+    Project     = "aws-infra"
   }
 }
 
 resource "aws_subnet" "myapp-subnet-1" {
-  vpc_id            = aws_vpc.myapp-vpc.id
-  cidr_block        = var.subnet_cidr_block
+  vpc_id = aws_vpc.myapp-vpc.id
+  cidr_block = var.vpc_cidr_block
   availability_zone = var.avail_zone
   tags = {
     Name = "${var.env_prefix}-subnet-1"
@@ -22,6 +24,7 @@ resource "aws_internet_gateway" "myapp-igw" {
 }
 
 resource "aws_default_route_table" "main-rtb" {
+  #Take the default route table that AWS created for this VPC and modify it
   default_route_table_id = aws_vpc.myapp-vpc.default_route_table_id
   route {
     cidr_block = "0.0.0.0/0"
@@ -34,24 +37,31 @@ resource "aws_default_route_table" "main-rtb" {
 
 resource "aws_default_security_group" "default-sg" {
   vpc_id = aws_vpc.myapp-vpc.id
+
+  #Anyone from internet → SSH into your server
   ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
+    from_port =22
+    to_port = 22
+    protocol = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
+    #cidr_blocks = ["YOUR_IP/32"]
   }
+  #Anyone → access your application (like Tomcat, Spring Boot)
   ingress {
     from_port   = 8080
     to_port     = 8080
     protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["0.0.0.0/0"] #Anyone in the world can access your server
   }
+
+  #All outbound traffic from your resource to anywhere on the internet
   egress {
     from_port   = 0
     to_port     = 0
-    protocol    = "-1"
+    protocol    = "-1" #ALL protocols allowed
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   tags = {
     Name = "${var.env_prefix}-default-sg"
   }
